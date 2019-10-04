68d95e9a7a19b61fdf1e89d27739e5c65217dac5
HiroIshida <spitfire.docomo@gmail.com>
2019-10-04 11:41:17 +0900

    add base_scan to launch

--- script/base_scan.py
+++ script/base_scan.py
@@ -12,7 +12,6 @@ rospy.init_node('base_scan_analyzer')
 pub = rospy.Publisher('basescan_range_min', Float32, queue_size=1)
 
 def callback(msg):
-    print "hoge"
     ranges_ = np.array(msg.ranges)
     ranges = ranges_[np.where(~np.isnan(msg.ranges))]
     range_min = ranges.min()
