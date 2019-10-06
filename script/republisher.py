c345c2d91b2995fa003eee127d84873ea5e4459f
HiroIshida <spitfire.docomo@gmail.com>
2019-10-06 15:31:44 +0900

    publish all the time

--- script/republisher.py
+++ script/republisher.py
@@ -11,22 +11,26 @@ topic_type = PoseStamped
 
 rospy.init_node('republisher')
 
-# closure
-def gen_callback(topic_name, topic_type):
-    pub = rospy.Publisher(topic_name, topic_type, queue_size = 1)
-    global msg 
-    msg = None
-    def inner(msg_):
-        global msg
-        msg = msg_
-        pub.publish(msg)
-    return inner
+class Republisher:
+    def __init__(self, topic_name, topic_name_new, topic_type):
+        self.msg = None
+        self.sub = rospy.Subscriber(topic_name, topic_type, self._callback)
+        self.pub = rospy.Publisher(topic_name_new, topic_type, queue_size = 1)
 
-cb_fridge = gen_callback('fridge_pose', PoseStamped)
-cb_handle = gen_callback('handle_pose', PoseStamped)
+    def _callback(self, msg):
+        self.msg = msg
 
-rospy.Subscriber(topic_fridge_name, topic_type, cb_fridge)
-rospy.Subscriber(topic_handle_name, topic_type, cb_handle)
-rospy.spin()
+    def publish(self):
+        if self.msg is not None:
+            self.pub.publish(self.msg)
+
+rep1 = Republisher(topic_fridge_name, 'fridge_pose', PoseStamped)
+rep2 = Republisher(topic_handle_name, 'handle_pose', PoseStamped)
+
+r = rospy.Rate(10)
+while not rospy.is_shutdown():
+    rep1.publish()
+    rep2.publish()
+    r.sleep()
 
 
